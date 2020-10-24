614f21cbfe2de493f2068a2bf3a9ea591e080fa2
AJCWhite <alexanderwhite2600@gmail.com>
2020-10-24 11:49:04 +0100

    Update thread test.py

--- thread test.py
+++ thread test.py
@@ -1,4 +1,12 @@
 # We want to create a script to find lab resources and be able to find where they are in the lab enviroment.
 # A bit like an iDRAC opensource
 
+import os
+import socket
 print("hello")
+
+
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
+print(ip)
