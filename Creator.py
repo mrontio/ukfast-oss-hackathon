f55ba754b356722ecddd894b219848a8e6264529
meetowl <mishuk2000@gmail.com>
2020-10-25 09:37:36 +0000

    LabNodes are now stored in a Lab, in a lab list

--- Creator.py
+++ Creator.py
@@ -1,22 +1,30 @@
-import json
-from json import JSONEncoder
-from LabNode import LabNode
+import pickle
+import os
 
-class Encoder(JSONEncoder):
-    def default(self, o):
-        return o.__dict__
+def storeLabs(labList):
+        if labList != [] :
+                with open('.lab_explorer_cfg.p', 'wb') as pfile:
+                        pickle.dump(labList, pfile)
+        else :
+                try: 
+                        os.remove('.lab_explorer_cfg.p')
+                except:
+                        return
+        
 
-diction = {}
-diction['node'] = []
-diction['node'].append(Encoder().encode(LabNode('icx3', '49:49:::::00', '192.168.1.256')))
-diction['node'].append(Encoder().encode(LabNode('icx4', '43:49:::::00', '193.168.1.256')))
+def loadLabs():
+        try :
+                with open('.lab_explorer_cfg.p', 'rb') as pfile:
+                        return pickle.load(pfile)
+        except:
+                return []
 
-with open('.lab_explorer_cfg.json', 'w') as jsonfile:
-        json.dump(diction, jsonfile)
+
+                
 
 
-with open('.lab_explorer_cfg.json', 'r') as jsonfile:
-        data = json.load(jsonfile)
-        print(data['node'])
-        # for p in data:
-        #         print(p['node'])
+                
+
+
+
+
--- LabNode.py
+++ LabNode.py
@@ -1,19 +1,36 @@
-from json import JSONEncoder
-
 class LabNode:
     def __init__(self, name, mac_addr, ip_addr):
         self.name = name
         self.mac_addr = mac_addr
-        self.ip_addr = ip_addr;
-        
+        self.ip_addr = ip_addr
+        self.labPosX = None
+        self.labPosY = None
 
-    def ipmi_status(self):
-        print("polling ipmi from machine", self.name)
+    def ipmiStatus(self):
+        print('polling ipmi from machine', self.name)
         # Do IPMI magic and return
         return 'being real cool'
 
-    def give_desc(self, desc):
+    def setDesc(self, desc):
         self.desc = desc
+
+    def setLabPosition(self, x, y):
+        self.labPosX = x
+        self.labPosY = y
+
+class Lab:
+    def __init__(self, name, height, width):
+        self.name = name
+        self.nodeList = []
+        self.height = height
+        self.width = width
+
+    def addNode(self, name, mac_addr, ip_addr):
+        self.nodeList.append(LabNode(name, mac_addr, ip_addr))
+
+    def getNode(self, i):
+        return nodeList[i]
+    
     
     
         
