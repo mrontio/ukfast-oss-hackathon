2d53898da2edf8c31c40d513e11445e76bae9801
meetowl <mishuk2000@gmail.com>
2020-10-25 15:29:53 +0000

    add ipmi interface

---
+++ IPMI.py
@@ -0,0 +1,12 @@
+from LabNode import LabNode
+
+def establishConnection(node):
+         
+def ipmiPowerOn(node):
+        
+def ipmiBlink(node):
+
+def ipmiPowerOff(node):
+
+def ipmiReboot(node):
+        
