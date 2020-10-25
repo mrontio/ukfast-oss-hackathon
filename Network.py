a64948705536043af1ed9b50d131c583282b9074
AJCWhite <alexanderwhite2600@gmail.com>
2020-10-25 14:03:01 +0000

    update to network py

--- Network.py
+++ Network.py
@@ -1,17 +1,23 @@
-# We want to create a script to find lab resources and be able to find where they are in the lab enviroment.
-# A bit like an iDRAC opensource
-
-# define list
-ip_dict = {}
-ip_list = []
-network_list = []
-host_ip = []
-network_count = 0
-# python sockets (to find current ip address or network segment you want to discover)
 import os
 import socket
 import subprocess
 import concurrent.futures
+from getmac import get_mac_address
+from LabNode import LabNode
+
+# define list
+ip_dict = {}
+ip_list = []
+mac_list = []
+network_list = []
+network_scan = []
+host_ip = []
+network_count = 0
+
+if os.name == "nt":
+    os = "win"
+else:
+    os = "linux"
 
 for i in range(0, 3):
     split_ip = socket.gethostbyname(socket.gethostname()).split('.')[i]
@@ -22,41 +28,57 @@ host_ip = host_ip + "."
 
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
+
 # print(host_ip, ip)
 for i in range(0, 254):
     network_list.append(host_ip + str(i))
 
 print("Discovering devices...")
 
-def discover(ip):
 
+def discoverwin(ip):
     output = subprocess.Popen(["ping.exe", ip], stdout=subprocess.PIPE).communicate()[0]
     if b'unreachable' in output:
         pass
-        #print(ip, "Offline")
-        #ip_dict[ip] = "Offline"
+        print(ip, "Offline")
+        # ip_dict[ip] = "Offline"
     else:
-        #print(ip, "Online")
         ip_dict[ip] = "Online"
         ip_list.append(ip)
+        ip_mac = get_mac_address(ip=ip)
+        mac_list.append(ip_mac)
+        try:
+            test_ip = socket.gethostbyaddr(ip)
+            print(ip, "Online", ip_mac, test_ip[0])
+            network_scan.append(LabNode(test_ip[0], ip_mac, ip))
+        except:
+            print(ip, "Online", ip_mac)
+            network_scan.append(LabNode("none", ip_mac, ip))
+            pass
 
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=300) as executor:
-    executor.map(discover, network_list)
+def discoverlinux(ip):
+    status, result = sp.getstatusoutput("ping -c1 -w2 " + ip)
+    if status == 0:
+        print("System " + ip + " is UP !")
+        ip_dict[ip] = "Online"
+        ip_list.append(ip)
+    else:
+        print("System " + ip + " is DOWN !")
+
+
+if os == "win":
+    with concurrent.futures.ThreadPoolExecutor(max_workers=300) as executor:
+        executor.map(discoverwin, network_list)
+elif os == "linux:":
+    with concurrent.futures.ThreadPoolExecutor(max_workers=300) as executor:
+        executor.map(discoverlinux, network_list)
+
+print(len(mac_list), mac_list)
 
 # for each ip:
 print("Number of discoverable devices: ", len(ip_list))
 for key in (ip_dict):
     print(key, '->', ip_dict[key])
-
-# incert into table to host infomation and uuid
-
-# give list of actions against list of nodes
-
-# - IPMI chassis LED
-
-# - get os info
-
-# - list lsusb to see if the device plugged in to which node
 
 exit()
