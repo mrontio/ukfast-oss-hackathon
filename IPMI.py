7fc6845889711cfb21d710cc7e1d80e239fbcbfb
AJCWhite <alexanderwhite2600@gmail.com>
2020-10-25 15:57:42 +0000

    updated with new IPMI

--- IPMI.py
+++ IPMI.py
@@ -1,15 +1,11 @@
 from LabNode import LabNode
+import subprocess
+import subprocess as sp
 
-def establishConnection(node):
+def establishConnection():
+    status, result = sp.getstatusoutput("ipmitool -H server-ipmi -U root -p local*99 chassis identify 1")
+    print(status)
+    print(result)
 
 
-         
-def ipmiPowerOn(node):
-        
-def ipmiBlink(node):
-        
-
-def ipmiPowerOff(node):
-
-def ipmiReboot(node):
-        
+establishConnection()
