d369243b5aef4aee3a47f9bc96fe88f6668241a9
meetowl <mishuk2000@gmail.com>
2020-10-25 14:38:55 +0000

    Menu & rack view!

--- Drawer.py
+++ Drawer.py
@@ -4,6 +4,7 @@ from LabNode import LabNode
 from LabNode import Lab
 
 
+
 def printLabs(labList):
         i = 1
         for lab in labList:
@@ -12,46 +13,71 @@ def printLabs(labList):
 def printLabTopdown(lab):
         rackRow = []
         string = ''
+        numeration = 1
         for y in range(1, int(lab.height) + 1):
                 rack_iterator = 0
                 for x in range(0, int(lab.width)):
-                         string += ('***************')
-                         string += ('     ')
+                        string += ('    ***************')
+                        string += ('         ')
+                        numeration += 1
                 string += '\n'
 
                 rackList = []
                 for i in range(1, int(lab.width) + 1):
                         print(i)
                         rackList.append(lab.getRack(y, i))
-
+                        
                 for i in range(0,4):
                         for rack in rackList:
                                 node = rack.getNode(i)
                                 if node != None:
-                                        string += '* {0:11} *'.format(node.name)
+                                        string += '{1:2}: * {0:11} *'.format(node.name, rack.num)
                                 else:
-                                        string += '*             *'
-                                string += '     '
+                                        string += '{0:2}: *             *'.format(rack.num)
+                                string += '         '
                         string += '\n'
                         
 
                 for x in range(0, int(lab.width)):
-                        string += '***************'
-                        string += '     '
-                string = string + '\n'                        
+                        string += '    ***************'
+                        string += '         '
+                string = string + '\n'
                         
                 
         print(string)
                 
                         
                 
+def printHelpLab():
+        print('v    view rack')
+        print('h    view help')
+        print('l    change lab')
+        print('n    create new lab')
+        print('q    quit')
 
+def createRackView(num, lab):
+        rack = lab.getRackByNum(num)
+        print('*----------------------------------------*')
+        for node in rack.nodeList:
+                print('* %10s: %10s, %10s  *' % (node.name, node.ip_addr, node.mac_addr))
+        print('*----------------------------------------*')
+        
+        
 def createLabView(labList):
         printLabs(labList)
         labNum = int(input('Which lab would you like to view? '))
         lab = labList[labNum - 1]
         printLabTopdown(lab)
-        
+        finished = False
+        while not finished :
+                option = input('> ')                
+                if (option == ''):
+                        printHelpLab()
+                elif (option == 'v'):
+                        rackNum = int(input("Which rack?"))
+                        createRackView(rackNum, lab)
+                elif (option == 'q'):
+                        finished = True
 
                         
 def dummyNetworkScan(begin, end, submask):
